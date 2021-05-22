324c01bce0df741bb2b954620f52b5047c54765e
b-stem-survey <ray-leun2@gmail.com>
2021-05-22 01:25:29 -0700

    Updates

--- src/components/Thanks.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export default () => {
-  return <div className='thanks-container'>
-    <i className='fas fa-check-circle'></i>
-    <p>Thank you for your submission!</p>
-  </div>
-}
